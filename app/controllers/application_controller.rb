2c35c1f40195af7f87e19fef0ffb86d440354963
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-07-03 21:54:37 +0800

    fix some bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
 
   def require_is_admin
     if !current_user.admin?
-      redirect_to root_path, alert: "You are not admin!"
+      flash[:alert] = "You are not admin!"
+      redirect_to root_path
     end
   end
-  
+
 end
